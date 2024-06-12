1c4a1580be36970a7470cec362265cb9ae9150b5
Maryam Naveed <maryaam2209@gmail.com>
2024-06-12 22:17:01 +0500

    remove: endpoints and baseurl from qr code verification

--- components/data/content/QR_Code_Verification/index.jsx
+++ components/data/content/QR_Code_Verification/index.jsx
@@ -1,7 +1,5 @@
 import Overview from "./QR_Code_Verification";
 import Authentication from "./Authentication";
-import BaseURL from "./BaseURL";
-import Endpoints from "./Endpoints";
 import QR_Code_Verification_Invalid from "./QR_Code_Verification_Invalid";
 import QR_Code_Verification_Valid from "./QR_Code_Verification_Valid";
 import QR_Code_Verification_Valid2 from "./QR_Code_Verification_Valid2";
@@ -11,8 +9,6 @@ import Events from "./Events";
 const API4 = {
   Overview,
   Authentication,
-  BaseURL,
-  Endpoints,
   QR_Code_Verification_Invalid,
   QR_Code_Verification_Valid,
   QR_Code_Verification_Valid2,
